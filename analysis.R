8df0be90ecb70c3a55272261127377982099ffe9
Joseph Ubelhart <ubelhj@uw.edu>
2018-05-18 13:24:55 -0700

    Gets top 200 songs

--- analysis.R
+++ analysis.R
@@ -8,13 +8,17 @@ spotify_auth <- oauth_endpoint(
   access = "https://accounts.spotify.com/api/token"
 )
 myapp <- oauth_app(client_id, client_id, client_secret)
-keys <-  oauth2.0_token(spotify_auth, myapp, scope = "playlist-read-private")
+keys <- oauth2.0_token(spotify_auth, myapp, scope = "playlist-read-private")
 header_key <- paste(
   keys$credentials$token_type, 
   keys$credentials$access_token
 )
 
 base_uri <- "https://api.spotify.com/v1/"
+
+download.file("https://spotifycharts.com/regional/global/daily/latest/download", destfile = "top200.csv")
+top_200 <- read.csv("top200.csv")
+
 
 ## Example to find data (Featured Playlist as example)
 browse_playlist_endpoint <- paste0(base_uri, "browse/featured-playlists")
@@ -23,5 +27,12 @@ playlists <- GET(
   add_headers("Authorization" = header_key)
 )
 
-content <- content(playlists)
+playlist_content <- content(playlists)
+
+top_list_uri <- paste0(base_uri, "browse/categories/toplists")
+top_json <- GET(
+  url = top_list_uri, 
+  add_headers("Authorization" = header_key)
+)
+top_list_content <- content(categories)
 
