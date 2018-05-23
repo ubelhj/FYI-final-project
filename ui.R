5b373e814ad387493a5d49b9e458644de9225264
James Kim <jamesk97@uw.edu>
2018-05-23 14:52:08 -0700

    Worked on About Page

--- ui.R
+++ ui.R
@@ -24,37 +24,53 @@ ui <- navbarPage("Spotify API (Temporary Name)",
             )
           )
         ),
-        tabPanel("Page 2",
-                 sidebarLayout(
-                   sidebarPanel(
-                     p("temp")
-                   ),
-                   mainPanel(
-                     p("temp")
-                   )
-                 )
+        tabPanel("Page 1",
+          sidebarLayout(
+            sidebarPanel(
+              p("temp")
+            ),
+            mainPanel(
+              p("temp")
+            )
+          )
         ),
         tabPanel("Page 3",
-                 sidebarLayout(
-                   sidebarPanel(
-                     p("temp")
-                   ),
-                   mainPanel(
-                     p("temp")
-                   )
-                 )
+          sidebarLayout(
+            sidebarPanel(
+              p("temp")
+            ),
+            mainPanel(
+             p("temp")
+            )
+          )
         ),
         tabPanel("Page 4",
-                 sidebarLayout(
-                   sidebarPanel(
-                     p("temp")
-                   ),
-                   mainPanel(
-                     p("temp")
-                   )
-                 )
+          sidebarLayout(
+            sidebarPanel(
+              p("temp")
+            ),
+            mainPanel(
+             p("temp")
+            )
+          )
+        ),
+        tabPanel("About",
+          splitLayout(
+            tabsetPanel(type = "tabs", 
+              tabPanel("About the Data Set"
+              ),
+              tabPanel("About the Team",
+                h3(strong("Project Presented By: Team AD3 - FYI (For Your Informatics)")),
+                h3("Members: James Kim, Joe Ubelhart, Timmy Tang, Owen DeArmond-MacLeod"),
+                h3("Biography:"),
+                h5(strong("James Kim:")),
+                h5(strong("Joe Ubelhart:")),
+                h5(strong("Timmy Tang:")),
+                h5(strong("Owen DeArmond-MacLeod:"))
+              )
+            )    
+          )
         )
-  
-)
+      )
 
 shinyUI(ui)
