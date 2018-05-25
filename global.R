bf74d8eaff7238edac14f3f2235f6c39838cf132
ubelhj <ubelhj@uw.edu>
2018-05-25 14:44:44 -0700

    Combines Audio analysis and ranking data

--- global.R
+++ global.R
@@ -7,9 +7,6 @@ library(plotly)
 source("apikeys.R")
 
 ## Authorization
-prev_oauth <- "./.httr-oathr"
-if (file.exists(prev_oauth)) file.remove(prev_oauth)
-
 spotify_auth <- oauth_endpoint(
   authorize = "https://accounts.spotify.com/authorize",
   access = "https://accounts.spotify.com/api/token"
@@ -44,9 +41,15 @@ this_is_america <- GET(
 )
 america_content <- content(this_is_america)
 
-ids <- strsplit(head(top_200$URL, 100), "/")
-id_list <- sapply(ids, "[[", 5)
-id_query <- paste0(id_list, collapse = ",")
+
+
+## Gets top 100 songs and combines with audio analysis
+
+top_100 <- top_200 %>% 
+  head(100) %>% 
+  mutate(id = sapply(strsplit(URL, "/"), "[[", 5))
+
+id_query <- paste0(top_100$id, collapse = ",")
 
 song_list_endpoint <- paste0(base_uri, "audio-features")
 top_100_songs <- GET(
@@ -54,5 +57,12 @@ top_100_songs <- GET(
   query = list("ids" = id_query),
   add_headers("Authorization" = header_key)
 )
+
 top_100_content <- content(top_100_songs)
 
+top_100_df <- do.call(rbind.data.frame, top_100_content[[1]])
+
+top_100_df <- right_join(top_100_df, top_100, by = "id")
+
+# ggplot(top_100_df, aes(Position, tempo, size = Streams, color = time_signature)) +
+#   geom_point()
