c6ea9ad9451922bd050e0468c6611178990111d2
James Kim <jamesk97@uw.edu>
2018-05-28 16:41:42 -0700

    Edits

--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
 source("global.R")
 
-ui <- navbarPage("Exploring Music Through Spotify", 
+ui <- navbarPage("Exploring Music", 
         tabPanel("Home", 
           sidebarLayout(position = "right",
             sidebarPanel(
@@ -10,24 +10,21 @@ ui <- navbarPage("Exploring Music Through Spotify",
                 href = 'https://beta.developer.spotify.com/documentation/web-api/')
             ),
             mainPanel(
-              #img(src="/logos/3.png"),
               h1(strong("Introduction:")),
-              p("Exploring Music utilitizes the Spotify Web API. The API endpoints
+              p(em("Exploring Music"), "utilitizes the Spotify Web API. The API endpoints
                 return JSON metadata about music artists, albums, BPM,
                 and tracks, directly from the Spotify Data Catalogue.
                 It also provides user-related data like playlists and
                 music that the user saves in the Your Music library."),
               p("From this information, our group decided to explore various
                 questions regarding music. Our project allows our users to
-                investigate genre popularity and album information to more
-                technical aspect of songs such as time signature vs. BPM and if
-                songs are more relatively positive and negative."),
-              p("Project Presented By: Team AD3 - FYI (For Your Informatics)")
-              #img(src="https://ischool.uw.edu/sites/default/files/inline-images/logo-black-vertical.jpg")
-            )
+                investigate broadly such as genre popularity and album information
+                to more technical aspect of songs such as danceability vs. energy
+                and if songs are more relatively positive and negative."),
+              p(strong("Project Presented By: Team AD3 - FYI (For Your Informatics)"))            )
           )
         ),
-        tabPanel("Audio Traits of Top 100 Songs",
+        tabPanel("Audio Traits",
           sidebarLayout(
             sidebarPanel(
               h4(strong("Directions")),
@@ -47,18 +44,23 @@ ui <- navbarPage("Exploring Music Through Spotify",
               p(htmlOutput("definition_two"))
             ),
             mainPanel(
+              h2("Audio Traits of the Top 100 Songs"),
               p(htmlOutput("statement_one")),
               tabsetPanel(type = "tabs",
                           tabPanel("Visual", 
                                    p("  "),
                                    plotlyOutput("plot")),
-                          tabPanel("Table", DT::dataTableOutput("table_one")),
-                          tabPanel("Summary", DT::dataTableOutput("summary"))
+                          tabPanel("Table", 
+                                   p("  "),
+                                   DT::dataTableOutput("table_one")),
+                          tabPanel("Summary", 
+                                   p("  "),
+                                   DT::dataTableOutput("summary"))
               )
             )
           )
         ),
-        tabPanel("Chloropleth Map",
+        tabPanel("Map",
           sidebarLayout(
             sidebarPanel(
               p("temp")
@@ -84,9 +86,14 @@ ui <- navbarPage("Exploring Music Through Spotify",
               tabPanel("About the Project",
                 h2(strong("Project Description")),
                 h4(strong("What is the data?")),
-                p("temp"),
+                HTML("The data utilizes Spotify's Web API. The API endpoints
+                     <br>return JSON metadata about music artists, albums, BPM,
+                     <br>and tracks, directly from the Spotify Data Catalogue."),
                 h4(strong("Why do we care?")),
-                p("temp"),
+                HTML("Our project allows our users to
+                     <br>investigate broadly such as genre popularity and album information
+                     <br>to more technical aspect of songs such as danceability vs. energy
+                     <br>and if songs are more relatively positive and negative."),
                 h2(strong("Technical Description")),
                 h4(strong("What libraries were used?")),
                 HTML("<li> DT - R interface to the JavaScript library DataTables. </li> 
@@ -99,14 +106,17 @@ ui <- navbarPage("Exploring Music Through Spotify",
                      apps straight from R. </li>
                      <li> ggplot2 - Data visualization package for R. </li>
                      <li> Plotly - Library makes interactive, publication-quality 
-                     graphs with R. </li>")
+                     graphs with R. </li>"),
+                h4(strong("Sources")),
+                HTML("<li> Stack Overflow </li> 
+                     <li> Plotly Documentation. </li>")
               ),
               tabPanel("About the Team",
                 h3(strong("Project Presented By: Team AD3 - FYI (For Your Informatics)")),
                 h3("Members: James Kim, Joe Ubelhart, Timmy Tang, Owen DeArmond-MacLeod"),
                 h3("Biography:"),
                 h5(strong("James Kim:")),
-                p("Hello! I like to code. Cx"),
+                p("Hello! I like to code."),
                 h5(strong("Joe Ubelhart:")),
                 p(""),
                 h5(strong("Timmy Tang:")),
